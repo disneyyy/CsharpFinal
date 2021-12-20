ccce75348beb2afa6e4307982bbf05fd0a26135a
lynn9106 <e94091071@gs.ncku.edu.tw>
2021-12-20 20:42:58 +0800

    pet

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -37,13 +37,27 @@
             this.buttonStart = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.labelTime = new System.Windows.Forms.Label();
+            this.pic_cat = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.point_Label = new System.Windows.Forms.Label();
+            this.level_Label = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.breed_Label = new System.Windows.Forms.Label();
+            this.message = new System.Windows.Forms.Label();
+            this.save_btn = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.load_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_cat)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonGame
             // 
-            this.buttonGame.Location = new System.Drawing.Point(553, 22);
+            this.buttonGame.Location = new System.Drawing.Point(761, 32);
+            this.buttonGame.Margin = new System.Windows.Forms.Padding(4);
             this.buttonGame.Name = "buttonGame";
-            this.buttonGame.Size = new System.Drawing.Size(180, 121);
+            this.buttonGame.Size = new System.Drawing.Size(248, 170);
             this.buttonGame.TabIndex = 0;
             this.buttonGame.Text = "遊戲";
             this.buttonGame.UseVisualStyleBackColor = true;
@@ -52,9 +66,10 @@
             // 
             // buttonSchedule
             // 
-            this.buttonSchedule.Location = new System.Drawing.Point(335, 22);
+            this.buttonSchedule.Location = new System.Drawing.Point(460, 32);
+            this.buttonSchedule.Margin = new System.Windows.Forms.Padding(4);
             this.buttonSchedule.Name = "buttonSchedule";
-            this.buttonSchedule.Size = new System.Drawing.Size(180, 121);
+            this.buttonSchedule.Size = new System.Drawing.Size(248, 170);
             this.buttonSchedule.TabIndex = 0;
             this.buttonSchedule.Text = "讀書計畫表";
             this.buttonSchedule.UseVisualStyleBackColor = true;
@@ -62,9 +77,10 @@
             // 
             // buttonCalculator
             // 
-            this.buttonCalculator.Location = new System.Drawing.Point(553, 177);
+            this.buttonCalculator.Location = new System.Drawing.Point(761, 248);
+            this.buttonCalculator.Margin = new System.Windows.Forms.Padding(4);
             this.buttonCalculator.Name = "buttonCalculator";
-            this.buttonCalculator.Size = new System.Drawing.Size(180, 121);
+            this.buttonCalculator.Size = new System.Drawing.Size(248, 170);
             this.buttonCalculator.TabIndex = 0;
             this.buttonCalculator.Text = "小算盤";
             this.buttonCalculator.UseVisualStyleBackColor = true;
@@ -72,9 +88,10 @@
             // 
             // buttonNote
             // 
-            this.buttonNote.Location = new System.Drawing.Point(335, 177);
+            this.buttonNote.Location = new System.Drawing.Point(460, 248);
+            this.buttonNote.Margin = new System.Windows.Forms.Padding(4);
             this.buttonNote.Name = "buttonNote";
-            this.buttonNote.Size = new System.Drawing.Size(180, 121);
+            this.buttonNote.Size = new System.Drawing.Size(248, 170);
             this.buttonNote.TabIndex = 0;
             this.buttonNote.Text = "日記";
             this.buttonNote.UseVisualStyleBackColor = true;
@@ -88,18 +105,20 @@
             "60",
             "90",
             "120"});
-            this.comboBox1.Location = new System.Drawing.Point(376, 370);
+            this.comboBox1.Location = new System.Drawing.Point(517, 518);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.Size = new System.Drawing.Size(165, 29);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.Text = "60";
             this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(532, 366);
+            this.buttonStart.Location = new System.Drawing.Point(732, 513);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(4);
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(83, 29);
+            this.buttonStart.Size = new System.Drawing.Size(114, 40);
             this.buttonStart.TabIndex = 2;
             this.buttonStart.Text = "開始讀書";
             this.buttonStart.UseVisualStyleBackColor = true;
@@ -114,17 +133,132 @@
             // 
             this.labelTime.AutoSize = true;
             this.labelTime.Font = new System.Drawing.Font("新細明體", 20F);
-            this.labelTime.Location = new System.Drawing.Point(645, 359);
+            this.labelTime.Location = new System.Drawing.Point(887, 502);
+            this.labelTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelTime.Name = "labelTime";
-            this.labelTime.Size = new System.Drawing.Size(129, 34);
+            this.labelTime.Size = new System.Drawing.Size(176, 47);
             this.labelTime.TabIndex = 3;
             this.labelTime.Text = "01:00:00";
             // 
+            // pic_cat
+            // 
+            this.pic_cat.Location = new System.Drawing.Point(68, 206);
+            this.pic_cat.Margin = new System.Windows.Forms.Padding(4);
+            this.pic_cat.Name = "pic_cat";
+            this.pic_cat.Size = new System.Drawing.Size(310, 427);
+            this.pic_cat.TabIndex = 4;
+            this.pic_cat.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 51);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "知識:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 89);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "等級:";
+            // 
+            // point_Label
+            // 
+            this.point_Label.AutoSize = true;
+            this.point_Label.Location = new System.Drawing.Point(127, 51);
+            this.point_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.point_Label.Name = "point_Label";
+            this.point_Label.Size = new System.Drawing.Size(20, 21);
+            this.point_Label.TabIndex = 7;
+            this.point_Label.Text = "0";
+            // 
+            // level_Label
+            // 
+            this.level_Label.AutoSize = true;
+            this.level_Label.Location = new System.Drawing.Point(127, 89);
+            this.level_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.level_Label.Name = "level_Label";
+            this.level_Label.Size = new System.Drawing.Size(20, 21);
+            this.level_Label.TabIndex = 8;
+            this.level_Label.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 126);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 21);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "品種:";
+            // 
+            // breed_Label
+            // 
+            this.breed_Label.AutoSize = true;
+            this.breed_Label.Location = new System.Drawing.Point(127, 126);
+            this.breed_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.breed_Label.Name = "breed_Label";
+            this.breed_Label.Size = new System.Drawing.Size(94, 21);
+            this.breed_Label.TabIndex = 10;
+            this.breed_Label.Text = "普通貓貓";
+            // 
+            // message
+            // 
+            this.message.AutoSize = true;
+            this.message.Font = new System.Drawing.Font("微軟正黑體", 11.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.message.Location = new System.Drawing.Point(26, 206);
+            this.message.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(150, 34);
+            this.message.TabIndex = 11;
+            this.message.Text = "開始讀書ㄅ";
+            // 
+            // save_btn
+            // 
+            this.save_btn.Location = new System.Drawing.Point(58, 157);
+            this.save_btn.Name = "save_btn";
+            this.save_btn.Size = new System.Drawing.Size(75, 45);
+            this.save_btn.TabIndex = 12;
+            this.save_btn.Text = "存檔";
+            this.save_btn.UseVisualStyleBackColor = true;
+            this.save_btn.Click += new System.EventHandler(this.save_btn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // load_btn
+            // 
+            this.load_btn.Location = new System.Drawing.Point(139, 157);
+            this.load_btn.Name = "load_btn";
+            this.load_btn.Size = new System.Drawing.Size(75, 45);
+            this.load_btn.TabIndex = 13;
+            this.load_btn.Text = "讀檔";
+            this.load_btn.UseVisualStyleBackColor = true;
+            this.load_btn.Click += new System.EventHandler(this.load_btn_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1100, 630);
+            this.Controls.Add(this.load_btn);
+            this.Controls.Add(this.save_btn);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.breed_Label);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.level_Label);
+            this.Controls.Add(this.point_Label);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pic_cat);
             this.Controls.Add(this.labelTime);
             this.Controls.Add(this.buttonStart);
             this.Controls.Add(this.comboBox1);
@@ -132,9 +266,11 @@
             this.Controls.Add(this.buttonSchedule);
             this.Controls.Add(this.buttonCalculator);
             this.Controls.Add(this.buttonGame);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_cat)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -150,6 +286,18 @@
         private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.PictureBox pic_cat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label point_Label;
+        private System.Windows.Forms.Label level_Label;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label breed_Label;
+        private System.Windows.Forms.Label message;
+        private System.Windows.Forms.Button save_btn;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button load_btn;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WindowsFormsApp1
 {
@@ -15,7 +16,11 @@ namespace WindowsFormsApp1
         int time = 0;//時間（秒數）
         bool study = false;
         int distracted = 0;//分心次數
+        int level = 1;//目前顯示圖片(貓貓等級)
+        int point = 0;//養成點數
+        bool newopen = true;//判斷重新開啟(for歡迎語)
         CalcForm calc;//計算機by電神Jacob
+
         public Form1()
         {
             InitializeComponent();
@@ -23,7 +28,8 @@ namespace WindowsFormsApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            pic_cat.Image = Image.FromFile(@"..\..\cats\cat" + level + ".png"); //目前貓咪圖片
+            pic_cat.SizeMode = PictureBoxSizeMode.Zoom;
         }
 
         private void buttonGame_Click(object sender, EventArgs e)//遊戲
@@ -41,7 +47,7 @@ namespace WindowsFormsApp1
             calc = new CalcForm();
             calc.Show();
         }
-        
+
         private void buttonNote_Click(object sender, EventArgs e)//日記
         {
 
@@ -49,12 +55,17 @@ namespace WindowsFormsApp1
 
         private void buttonStart_Click(object sender, EventArgs e)//按下開始讀書
         {
+            newopen = false;
+            load_btn.Enabled = false;
+
             time = int.Parse(comboBox1.Text) * 60;
             timer1.Enabled = true;
             buttonStart.Enabled = false;
             study = true;
             comboBox1.Enabled = false;
             distracted = 0;
+
+            message.Text = "讀書中~";
         }
 
         private void timer1_Tick(object sender, EventArgs e)//倒數計時
@@ -62,13 +73,17 @@ namespace WindowsFormsApp1
             time--;
             string temp = "0" + time / 3600 + ":";
             if (time / 60 % 60 < 10) temp += "0";
-            temp += time / 60 % 60; temp += ":"; 
+            temp += time / 60 % 60; temp += ":";
             if (time % 60 < 10) temp += "0";
-            labelTime.Text =  temp + time % 60;
+            labelTime.Text = temp + time % 60;
 
-            if(time < 0)
+            if (time < 0)
             {
-                //增加養成點數（可以用comboBox.Text來增加）
+                point += int.Parse(comboBox1.Text);point_Label.Text = Convert.ToString(point); //增加養成點數（用comboBox.Text來增加）
+                level = levelup(point, level,newopen);level_Label.Text = Convert.ToString(level);
+                pic_cat.Image = Image.FromFile(@"..\..\cats\cat" + level + ".png");//貓貓等級更新
+                load_btn.Enabled = true;
+
                 labelTime.Text = "01:00:00";
                 comboBox1.Text = "60";
                 timer1.Enabled = false;
@@ -86,8 +101,12 @@ namespace WindowsFormsApp1
                 MessageBox.Show("期末不想歐趴了484", "不准偷懶", MessageBoxButtons.OK);
                 distracted++;
                 //分心累積?次扣成長點數
-                if(distracted >= 5)//暫時隨便設的ㄏㄏ
+                if (distracted >= 5)//暫時隨便設的ㄏㄏ
                 {
+                    point -=30;
+                    level = levelup(point, level,newopen); level_Label.Text = Convert.ToString(level);
+                    if (point < 0) point = 0;
+                    point_Label.Text = Convert.ToString(point); 
                     buttonGame.Enabled = false;
                 }
             }
@@ -108,12 +127,12 @@ namespace WindowsFormsApp1
                 return;
             }
             int T = int.Parse(comboBox1.Text) * 60;
-            if(T > 6 * 60 * 60)
+            if (T > 6 * 60 * 60)
             {
                 MessageBox.Show("時間上限6小時！", "休息是為了走更長的路", MessageBoxButtons.OK);
                 T = 6 * 60 * 60;
                 comboBox1.Text = "360";
-            }else if(T < 0)
+            } else if (T < 0)
             {
                 MessageBox.Show("時間不能為負！", "錯誤", MessageBoxButtons.OK);
                 T = 0;
@@ -123,6 +142,93 @@ namespace WindowsFormsApp1
             string temp = "" + M;
             if (T / 60 % 60 < 10) temp = "0" + temp;
             labelTime.Text = "0" + T / 3600 + ":" + temp + ":" + "00";
+        }
+
+        public int levelup(int point,int level,bool newopen) //累積點數進化貓貓
+        {
+            int currentlevel = level;
+
+            if (point <= 0)
+            {
+                level = 0;
+                breed_Label.Text = "X";
+                message.Text = "感覺大事不喵";
+            }
+            else if (point > 0 && point <= 120){level = 1; breed_Label.Text = "普通貓貓";}//120
+            else if (point <= 240) { level = 2; breed_Label.Text = "眼鏡貓貓"; }//240
+            else if (point <= 420) { level = 3; breed_Label.Text = "學士帽貓貓"; }//420
+            else if (point <= 660) { level = 4; breed_Label.Text = "POP貓貓"; }//660
+            else if (point <= 1000) { level = 5; breed_Label.Text = "氣到咪咪貓貓"; }//1000
+            else { level = 6; breed_Label.Text = "奇怪的姿勢又增加ㄌ貓貓"; }
+
+            pic_cat.Image = Image.FromFile(@"..\..\cats\cat" + level + ".png");
+
+
+            if (newopen == true) { message.Text = "歡迎回來!"; return level; }
+
+
+                if (level>currentlevel)
+            {
+                if(currentlevel==0)
+                {
+                 message.Text = "是普通貓貓!";
+                }
+                else
+                 message.Text = "你的貓貓進化了!";
+            }
+                else if(currentlevel<level)
+            {
+                message.Text = "你的貓貓退化了...";
+            }
+                else if(currentlevel==level)
+            { message.Text = "繼續努力ㄅ!"; }
+
+            return level;
+        }
+
+        private void save_btn_Click(object sender, EventArgs e)
+        {
+            String stFilter = "TXT(*.txt)|*.txt";
+            saveFileDialog1.Filter = stFilter;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName))
+                {
+                    sw.WriteLine(point_Label.Text);
+                    sw.WriteLine(level_Label.Text);
+                    sw.WriteLine(breed_Label.Text);
+                }
+            }
+        }
+
+        private void load_btn_Click(object sender, EventArgs e)
+        {
+            String stFilter = "TXT(*.txt)|*.txt";
+            openFileDialog1.Filter = stFilter;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string[] mydata = new string[3];
+
+                int k = 0;
+                StreamReader sr = new StreamReader(openFileDialog1.FileName);
+                do
+                {
+                    string line = sr.ReadLine();                //逐行讀取清單中歌曲路徑
+                    if (sr.Peek() == -1) break;
+                    mydata[k] = line;
+                    k++;
+                } while (true);
+
+                point_Label.Text = mydata[0];
+                level_Label.Text = mydata[1];
+                breed_Label.Text = mydata[2];
+                point = int.Parse(mydata[0]);
+                level = int.Parse(mydata[1]);
+                newopen = true;
+
+                levelup(point, level, newopen);
+
+            }
         }
     }
 }
